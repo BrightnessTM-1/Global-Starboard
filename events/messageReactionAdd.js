c85e08af4400e0a14723d8914e8482d768ba03b2
Brightness™ <46763431+BrightnessTM@users.noreply.github.com>
2019-06-25 10:59:38 -0400

    Reaction add on message

--- events/messageReactionAdd.js
+++ events/messageReactionAdd.js
@@ -1,4 +1,5 @@
 const configuration = require('../config.json');
+const core = require('../coreFunctions.js')
 module.exports = (client, Discord, messageReaction, user) => {
     if (messageReaction.emoji.name === "⭐") {
         //Star reaction
@@ -13,9 +14,39 @@ module.exports = (client, Discord, messageReaction, user) => {
             var reactedOnGlobal = client.messages.get(messageReaction.message.id, "starsInfo.global")
             var reactedOnServer = client.messages.get(messageReaction.message.id, "starsInfo.server")
             reactedOnMessage.push(user.id)
-            if (messageReaction.count > 0) {//Will be 6 later
+            var allReactions = reactedOnMessage.concat(reactedOnGlobal, reactedOnServer)
+            var reactionsNoDupe = Array.from(new Set(allReactions));
+            var starCount = reactionsNoDupe.length;
+            client.messages.set(messageReaction.message.id, starCount, "stars");
+            client.messages.set(messageReaction.message.id, reactedOnMessage, "starsInfo.message");
+            if (starCount > 0) {//Will be 6 later
+              if (!client.messages.get(messageReaction.message.id, "globalBoardMessage")) {
+                client.messages.set(messageReaction.message.id, true, "globalBoard");
+                
                 let embed = new Discord.RichEmbed()
-                console.log("starred")
+                embed.setTitle(messageReaction.message.author.tag + " in " + messageReaction.message.guild.name)
+                embed.setThumbnail(messageReaction.message.author.displayAvatarURL)
+                embed.setColor("#FFAD00")
+                embed.setTimestamp()
+                embed.setDescription(messageReaction.message.content)
+                client.channels.get(configuration.config.channels.global_starboard).send(":star: **" + starCount.toString() + "** | <#" + messageReaction.message.channel.id + ">", embed).then(sent => {
+                  client.messages.set(messageReaction.message.id, sent.id, "globalBoardMessage");
+                }).catch(e => {
+                  core.botLog("`[ERROR]` Failed to post message to Global Starboard")
+                })
+              } else {
+                let embed = new Discord.RichEmbed()
+                embed.setTitle(messageReaction.message.author.tag + " in " + messageReaction.message.guild.name)
+                embed.setThumbnail(messageReaction.message.author.displayAvatarURL)
+                embed.setColor("#FFAD00")
+                embed.setTimestamp()
+                embed.setDescription(messageReaction.message.content)
+                client.channels.get(configuration.config.channels.global_starboard).fetchMessage(client.messages.get(messageReaction.message.id, "globalBoardMessage")).then(fetched => {
+                  fetched.edit(":star: **" + starCount.toString() + "** | <#" + messageReaction.message.channel.id + ">", embed)
+                }).catch(e => {
+                  core.botLog("`[ERROR]` Failed to fetch message on Global Starboard")
+                })
+              }
             }
         }
     }
