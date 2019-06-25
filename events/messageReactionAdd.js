a0792c7d3ea5e7adc4ba79b418c5deecfae23ff4
Brightness™ <46763431+BrightnessTM@users.noreply.github.com>
2019-06-25 16:39:02 -0400

    Server starboard reaction adding

--- events/messageReactionAdd.js
+++ events/messageReactionAdd.js
@@ -13,7 +13,7 @@ module.exports = (client, Discord, messageReaction, user) => {
         if (messageReaction.message.channel.id === configuration.config.channels.global_starboard) {
             if (user.id === client.user.id) return; //ignore self
             var foundMsg = client.messages.find(msg => msg.globalBoardMessage == messageReaction.message.id).messageid
-
+            if (!foundMsg) return;
             var reactedOnMessage = client.messages.get(foundMsg, "starsInfo.message")
             var reactedOnGlobal = client.messages.get(foundMsg, "starsInfo.global")
             var reactedOnServer = client.messages.get(foundMsg, "starsInfo.server")
@@ -29,7 +29,7 @@ module.exports = (client, Discord, messageReaction, user) => {
                 embed.setTitle(fetched.author.tag + " in " + fetched.guild.name)
                 embed.setThumbnail(fetched.author.displayAvatarURL)
                 embed.setColor("#FFAD00")
-                embed.setTimestamp()
+                embed.setTimestamp(fetched.createTimestamp)
                 embed.setDescription(fetched.content)
               if (fetched.attachments.first()) {
                   embed.setImage(fetched.attachments.first().url)
@@ -40,10 +40,95 @@ module.exports = (client, Discord, messageReaction, user) => {
                   core.botLog("`[ERROR]` Failed to fetch message on Global Starboard")
                 })
             }
+              
+              if (client.messages.get(foundMsg, 'serverBoard') === true) {
+            let embed = new Discord.RichEmbed()
+                embed.setTitle(fetched.author.tag)
+                embed.setThumbnail(fetched.author.displayAvatarURL)
+                embed.setColor("#FFAD00")
+                embed.setTimestamp(fetched.createdTimestamp)
+                embed.setDescription(fetched.content)
+                embed.addField("Jump to Message", "[Jump to message](https://discordapp.com/channels/" + fetched.guild.id + "/" + fetched.channel.id + "/" + fetched.id + ")")
+                if (fetched.attachments.first()) {
+                  embed.setImage(fetched.attachments.first().url)
+                }
+                client.channels.get(client.servers.get(messageReaction.message.guild.id, 'starboard')).fetchMessage(client.messages.get(fetched.id, "serverBoardMessage")).then(fetched2 => {
+                  fetched2.edit(":star: **" + starCount.toString() + "** | <#" + fetched.channel.id + ">", embed)
+                }).catch(e => {
+                  core.botLog("`[ERROR]` Failed to fetch message on server Starboard")
+                })
+          }
             })
+          
+          
         
-        } else if (0 === 1) {
-            //Server starboard, not doing this yet
+        } else if (messageReaction.message.channel.id === client.servers.get(messageReaction.message.guild.id, 'starboard')) {
+            //Server starboard
+            if (user.id === client.user.id) return; //ignore self
+            var foundMsg = client.messages.find(msg => msg.serverBoardMessage == messageReaction.message.id).messageid
+            if (!foundMsg) return;
+            var reactedOnMessage = client.messages.get(foundMsg, "starsInfo.message")
+            var reactedOnGlobal = client.messages.get(foundMsg, "starsInfo.global")
+            var reactedOnServer = client.messages.get(foundMsg, "starsInfo.server")
+            reactedOnServer.push(user.id)
+            var allReactions = reactedOnMessage.concat(reactedOnGlobal, reactedOnServer)
+            var reactionsNoDupe = Array.from(new Set(allReactions));
+            var starCount = reactionsNoDupe.length;
+            client.messages.set(foundMsg, starCount, "stars");
+            client.messages.set(foundMsg, reactedOnServer, "starsInfo.server");
+            client.channels.get(client.messages.get(foundMsg, "channelid")).fetchMessage(client.messages.get(foundMsg, "messageid")).then(fetched => {
+            if (starCount >= client.servers.get(client.channels.get(client.messages.get(foundMsg, 'channelid')).guild.id).threshold) {
+                let embed = new Discord.RichEmbed()
+                embed.setTitle(fetched.author.tag)
+                embed.setThumbnail(fetched.author.displayAvatarURL)
+                embed.setColor("#FFAD00")
+                embed.setTimestamp(fetched.createdTimestamp)
+                embed.setDescription(fetched.content)
+                embed.addField("Jump to Message", "[Jump to message](https://discordapp.com/channels/" + fetched.guild.id + "/" + fetched.channel.id + "/" + fetched.id + ")")
+                if (fetched.attachments.first()) {
+                  embed.setImage(fetched.attachments.first().url)
+                }
+                client.channels.get(client.servers.get(client.channels.get(client.messages.get(foundMsg, 'channelid')).guild.id).starboard).fetchMessage(client.messages.get(fetched.id, "serverBoardMessage")).then(fetched_ss => {
+                  fetched_ss.edit(":star: **" + starCount.toString() + "** | <#" + fetched.channel.id + ">", embed)
+                }).catch(e => {
+                  core.botLog("`[ERROR]` Failed to fetch message on Server Starboard")
+                })
+            }
+            
+          
+          if (client.messages.get(foundMsg, "globalBoard") === true) {
+            let embed = new Discord.RichEmbed()
+                embed.setTitle(fetched.author.tag + " in " + fetched.guild.name)
+                embed.setThumbnail(fetched.author.displayAvatarURL)
+                embed.setColor("#FFAD00")
+                embed.setTimestamp(fetched.createdTimestamp)
+                embed.setDescription(fetched.content)
+                if (fetched.attachments.first()) {
+                  embed.setImage(fetched.attachments.first().url)
+                }
+                client.channels.get(configuration.config.channels.global_starboard).fetchMessage(client.messages.get(fetched.id, "globalBoardMessage")).then(fetchedgs => {
+                  fetchedgs.edit(":star: **" + starCount.toString() + "** | <#" + fetched.channel.id + ">", embed)
+                }).catch(e => {
+                  core.botLog("`[ERROR]` Failed to fetch message on Global Starboard")
+                })
+          } else if (starCount >= configuration.config.global_threshold) {
+             let embed = new Discord.RichEmbed()
+                embed.setTitle(fetched.author.tag + " in " + fetched.guild.name)
+                embed.setThumbnail(fetched.author.displayAvatarURL)
+                embed.setColor("#FFAD00")
+                embed.setTimestamp(fetched.createdTimestamp)
+                embed.setDescription(fetched.content)
+                if (fetched.attachments.first()) {
+                  embed.setImage(fetched.attachments.first().url)
+                }
+                client.channels.get(configuration.config.channels.global_starboard).send(":star: **" + starCount.toString() + "** | <#" + fetched.channel.id + ">", embed).then(sent => {
+                  client.messages.set(fetched.id, sent.id, "globalBoardMessage");
+                  sent.react("⭐")
+                }).catch(e => {
+                  core.botLog("`[ERROR]` Failed to post message to Global Starboard")
+                })
+          }
+            })
         } else {
             //Not on a starboard
             var reactedOnMessage = client.messages.get(messageReaction.message.id, "starsInfo.message")
@@ -55,7 +140,45 @@ module.exports = (client, Discord, messageReaction, user) => {
             var starCount = reactionsNoDupe.length;
             client.messages.set(messageReaction.message.id, starCount, "stars");
             client.messages.set(messageReaction.message.id, reactedOnMessage, "starsInfo.message");
-            if (starCount >= configuration.config.global_threshold) {//Will be 6 later
+            if (starCount >= client.servers.get(messageReaction.message.guild.id, 'threshold')) {
+              if (!client.messages.get(messageReaction.message.id, "serverBoardMessage")) {
+                client.messages.set(messageReaction.message.id, true, "serverBoard");
+                
+                let embed = new Discord.RichEmbed()
+                embed.setTitle(messageReaction.message.author.tag)
+                embed.setThumbnail(messageReaction.message.author.displayAvatarURL)
+                embed.setColor("#FFAD00")
+                embed.setTimestamp(messageReaction.message.createdTimestamp)
+                embed.setDescription(messageReaction.message.content)
+                embed.addField("Jump to Message", "[Jump to message](https://discordapp.com/channels/" + messageReaction.message.guild.id + "/" + messageReaction.message.channel.id + "/" + messageReaction.message.id + ")")
+                if (messageReaction.message.attachments.first()) {
+                  embed.setImage(messageReaction.message.attachments.first().url)
+                }
+                client.channels.get(client.servers.get(messageReaction.message.guild.id, 'starboard')).send(":star: **" + starCount.toString() + "** | <#" + messageReaction.message.channel.id + ">", embed).then(sent => {
+                  client.messages.set(messageReaction.message.id, sent.id, "serverBoardMessage");
+                  sent.react("⭐")
+                }).catch(e => {
+                  core.botLog("`[ERROR]` Failed to post message to server Starboard")
+                })
+              } else {
+                let embed = new Discord.RichEmbed()
+                embed.setTitle(messageReaction.message.author.tag)
+                embed.setThumbnail(messageReaction.message.author.displayAvatarURL)
+                embed.setColor("#FFAD00")
+                embed.setTimestamp(messageReaction.message.createdTimestamp)
+                embed.setDescription(messageReaction.message.content)
+                embed.addField("Jump to Message", "[Jump to message](https://discordapp.com/channels/" + messageReaction.message.guild.id + "/" + messageReaction.message.channel.id + "/" + messageReaction.message.id + ")")
+                if (messageReaction.message.attachments.first()) {
+                  embed.setImage(messageReaction.message.attachments.first().url)
+                }
+                client.channels.get(client.servers.get(messageReaction.message.guild.id, 'starboard')).fetchMessage(client.messages.get(messageReaction.message.id, "serverBoardMessage")).then(fetched => {
+                  fetched.edit(":star: **" + starCount.toString() + "** | <#" + messageReaction.message.channel.id + ">", embed)
+                }).catch(e => {
+                  core.botLog("`[ERROR]` Failed to fetch message on server Starboard")
+                })
+              }
+            }
+            if (starCount >= configuration.config.global_threshold) {
               if (!client.messages.get(messageReaction.message.id, "globalBoardMessage")) {
                 client.messages.set(messageReaction.message.id, true, "globalBoard");
                 
@@ -63,7 +186,7 @@ module.exports = (client, Discord, messageReaction, user) => {
                 embed.setTitle(messageReaction.message.author.tag + " in " + messageReaction.message.guild.name)
                 embed.setThumbnail(messageReaction.message.author.displayAvatarURL)
                 embed.setColor("#FFAD00")
-                embed.setTimestamp()
+                embed.setTimestamp(messageReaction.message.createdTimestamp)
                 embed.setDescription(messageReaction.message.content)
                 if (messageReaction.message.attachments.first()) {
                   embed.setImage(messageReaction.message.attachments.first().url)
@@ -79,7 +202,7 @@ module.exports = (client, Discord, messageReaction, user) => {
                 embed.setTitle(messageReaction.message.author.tag + " in " + messageReaction.message.guild.name)
                 embed.setThumbnail(messageReaction.message.author.displayAvatarURL)
                 embed.setColor("#FFAD00")
-                embed.setTimestamp()
+                embed.setTimestamp(messageReaction.message.createdTimestamp)
                 embed.setDescription(messageReaction.message.content)
                 if (messageReaction.message.attachments.first()) {
                   embed.setImage(messageReaction.message.attachments.first().url)
