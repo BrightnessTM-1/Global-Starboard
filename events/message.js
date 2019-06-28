c8ca69c10af851c63a6f7a450b7cb9de88e83ea0
Brightness™ <46763431+BrightnessTM@users.noreply.github.com>
2019-06-28 07:49:46 -0400

    Beautification

--- events/message.js
+++ events/message.js
@@ -5,9 +5,9 @@ const request = require('request');
 const configuration = require('../config.json');
 module.exports = (client, Discord, message) => {
   //Save as known to bot
-    if (!client.messages) {
-        core.initEnmap("messages", client)
-    }
+  if (!client.messages) {
+    core.initEnmap("messages", client)
+  }
   if (message.channel.type !== "text") return;
 
   client.messages.ensure(`${message.id}`, {
@@ -15,9 +15,9 @@ module.exports = (client, Discord, message) => {
     channelid: message.channel.id,
     stars: 0,
     starsInfo: {
-        global: [],
-        server: [],
-        message: []
+      global: [],
+      server: [],
+      message: []
     },
     reported: false,
     hidden: false,
@@ -29,20 +29,20 @@ module.exports = (client, Discord, message) => {
   //Check if message is a command
   fs.readdir("./commands/", (err, files) => {
     files.forEach(file => {
-        const commandName = file.split(".")[0]; //Command to check against
-        const command = require("../commands/" + commandName); //Command file
-        if (message.channel.type !== "text" || message.author.bot === true) return; //Ignore DMs and bots
+      const commandName = file.split(".")[0]; //Command to check against
+      const command = require("../commands/" + commandName); //Command file
+      if (message.channel.type !== "text" || message.author.bot === true) return; //Ignore DMs and bots
 
-        let commandText = message.content.split(" ")[0].toLowerCase() //Input command
-        if (commandText === configuration.config.prefix + commandName) { //Check if command matches
-            let args = message.content.split(" ").splice(1);
-            var permission = core.checkPermissions(message.author.id, client);
+      let commandText = message.content.split(" ")[0].toLowerCase() //Input command
+      if (commandText === configuration.config.prefix + commandName) { //Check if command matches
+        let args = message.content.split(" ").splice(1);
+        var permission = core.checkPermissions(message.author.id, client);
 
-            if (permission > command.controls.permission) return message.react("🚫");
-            if (command.controls.enabled === false) return message.reply("This command has been disabled globally.");
-            return command.do(message, client, args, Discord);
-        
-        }
+        if (permission > command.controls.permission) return message.react("🚫");
+        if (command.controls.enabled === false) return message.reply("This command has been disabled globally.");
+        return command.do(message, client, args, Discord);
+
+      }
     });
-    })
+  })
 };
